956d9779f8ded553097b1424d8d602fdaddaf393
viinay <viinay@users.noreply.github.com>
2017-08-09 10:50:35 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser') 
+var urlendoded = bodyParser.urlencoded({extended:false})
+
+var port = 3001
+app.listen(port,function(){
+	console.log('server running on port :',port)
+})
+
+app.use(bodyParser.json())
+app.use(urlendoded)
+
+app.get('/',function(req,res){
+	console.log('server set up is ready')
+	res.send('server set up is ready')
+})
