99ceaa5439ec2429a423ba4498efdd86061303cc
viinay <viinay@users.noreply.github.com>
2017-08-09 11:24:11 +0530

    Update angular-modules.ts

--- angular-modules.ts
+++ angular-modules.ts
@@ -1,1 +1,48 @@
 
+//to create Modules in Angular use
+/*
+NgModule
+--------
+
+use:
+*/
+import { NgModule } from '@angule/core'
+@NgModule({
+	imports:[
+		NgModule
+	]
+})
+
+
+//for HttpModule 
+/*
+HttpModule
+----------
+
+use:
+*/
+import { HttpModule } from '@angular/http';
+
+@NgModule({
+	imports:[
+		HttpModule
+	]
+})
+
+
+//for FormsModule
+/*
+FormsModule
+-----------
+
+use:
+*/
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+	import:[
+		FormsModule
+	]
+})
+
+
