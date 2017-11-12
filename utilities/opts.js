114ce9fcfd6b05a7aa76769424f16b6cfa39d1d5
viinay <viinay@users.noreply.github.com>
2017-11-13 00:37:22 +0530

    Update opts.js

--- utilities/opts.js
+++ utilities/opts.js
@@ -11,3 +11,17 @@ function validateProperties(required, options) {
 
 var r = validateProperties(['apple','orange'],{apple:'hai',obrange:'hai',banana:'be hai'})
 console.log(r)//false 
+
+
+function validateProperties(required, options, formatString, callback) {
+  return !required.some(function (item) {
+    if (typeof(options[item]) === 'undefined') {
+      errs.handle(
+        errs.create({ message: util.format(formatString, item) }),
+        callback
+      );
+      return true;
+    }
+    return false;
+  });
+}
