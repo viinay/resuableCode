d334b4a950f426f58ccaf3967682349b25138672
viinay <viinay@users.noreply.github.com>
2018-04-29 11:52:36 +0530

    Update nodejs-concepts.js

--- nodejs-concepts.js
+++ nodejs-concepts.js
@@ -2,7 +2,8 @@
 
 var a = Object.assign({}, b); //ES6 feature and currently not available in all browsers
 
-alternatively we can do->
+#alternatively we can do->
+
 let obj= {
     hello: 'hello'
 };
