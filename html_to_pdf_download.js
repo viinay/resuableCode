788b729822b83f7b5cc1ab65b5e570e4665602a5
viinay <viinay@users.noreply.github.com>
2018-05-18 18:44:10 +0530

    Create html_to_pdf_download.js

---
+++ html_to_pdf_download.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser') 
+var urlendoded = bodyParser.urlencoded({extended:false})
+
+var port = 3001
+app.listen(port,function(){
+	console.log('server running on port :',port)
+})
+
+app.use(bodyParser.json())
+app.use(urlendoded)
+
+app.get('/',function(req,res){
+	console.log('server set up is ready')
+	res.send('server set up is ready')
+})
+
+var fs = require('fs')
+var pdf = require('html-pdf'); 
+    
+app.get('/download',function(req,res){
+    pdf.create(fs.readFileSync('./file1.html', 'utf8')).toStream(function(err, stream){
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=file1.pdf');
+        stream.pipe(res);
+    });
+})
