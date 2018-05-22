af1402411e0df7333d76c5df9466398ef05447da
viinay <viinay@users.noreply.github.com>
2018-05-22 12:39:38 +0530

    Create dateDifferenceInDays.js

---
+++ dateDifferenceInDays.js
@@ -0,0 +1,5 @@
+var dateDiffInDays = function(date1,date2){
+    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+    return diffDays;
+}
