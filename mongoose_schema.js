574c0d2feea01f8947e8ad5443897f80e992f61f
viinay <viinay@users.noreply.github.com>
2018-05-24 12:46:09 +0530

    Update mongoose_schema.js

--- mongoose_schema.js
+++ mongoose_schema.js
@@ -1,2 +1,8 @@
 var Schema = new Schema({...}, { versionKey: false });
 var Schema = new Schema({ __v: { type: Number, select: false}})
+var Schema = new Schema({...},{
+	timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+	versionKey: false
+})
+
+mongoose.Schema.Types.Double
