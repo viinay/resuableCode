99f2136e0db7c544ed2f4443dfb3f1f88ead49dc
viinay <viinay@users.noreply.github.com>
2018-06-08 17:05:33 +0530

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,5 @@
+function getCookieValue(e) {
+    return document.cookie.length > 0 && (c_start = document.cookie.indexOf(e + "="), -1 != c_start) ? (c_start = c_start + e.length + 1, c_end = document.cookie.indexOf(";", c_start), -1 == c_end && (c_end = document.cookie.length), unescape(document.cookie.substring(c_start, c_end))) : ""
+}
+
+getCookieValue(cookieName)
